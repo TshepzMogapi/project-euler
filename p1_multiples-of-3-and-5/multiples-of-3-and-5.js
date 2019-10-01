0ac61177d5364f975c8e6a84edce04666c8c63a3
Tshepiso Mogapi <tshepisomogapi@Tshepisos-MacBook-Pro.local>
2019-10-01 22:36:24 +0200

    p1_js function

--- p1_multiples-of-3-and-5/multiples-of-3-and-5.js
+++ p1_multiples-of-3-and-5/multiples-of-3-and-5.js
@@ -1,3 +1,13 @@
-function multiplesOf3and5(number) {
-    return 1;
+function multiplesOf3and5(n) {
+
+  let sum = 0;
+  for(let i = 0; i < n; i++){
+
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
   }
+    return sum;
+  }
+
+  console.log(multiplesOf3and5(19564));
